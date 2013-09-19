cf1ff3ba7bb15b01fba4c93849eaf47ebb36e4a7
luismiv85 <louismiguelv85@gmail.com>
2013-09-19 16:51:33 +0200

    changes top start page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ RailsBoots::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'home#index'
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
