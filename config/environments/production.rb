d30ceff17b98ddcb744723bc79086183996259e2
luismiv85 <louismiguelv85@gmail.com>
2013-09-20 15:06:25 +0200

    configuration's style folders (ie, all) and FrontPage top finished

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ RailsBoots::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %w(application-ie.css.scss)
 
   # Generate digests for assets URLs.
   config.assets.digest = true
