e400a5f9f707d68c2e1cf9885d7c50bb0ad57693
luismiv85 <louismiguelv85@gmail.com>
2013-09-23 20:29:51 +0200

    solved problem turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 RailsBoots::Application.routes.draw do
   get "home/index"
+  get "home/prueba"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
